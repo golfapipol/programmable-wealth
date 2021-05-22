166cd40c1e1a5153ec70be8a762fe6ca35e01da7
Apipol Sukgler <golf.apipol@gmail.com>
2021-05-22 11:40:43 +0700

    config tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edited <code>src/App.js</code> and save to reload.
         </p>
